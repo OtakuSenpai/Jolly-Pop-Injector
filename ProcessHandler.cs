84140feb2fa46c3b1dd54af63b96776c228b7302
Austin <awilliams17411@gmail.com>
2017-12-06 20:06:10 -0600

    added code for processhandler

--- ProcessHandler.cs
+++ ProcessHandler.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Jolly_Pop_Injector {
-    class ProcessHandler {
+    public static class ProcessHandler {
+        public static bool ProcessIsRunning(string process_name) { //If user passes process name with ".exe",
+            if (process_name.Substring(process_name.Length - 4) == ".exe") {
+                process_name = process_name.Substring(0, process_name.Length - 4); //Strip it out
+            }
+            Process[] pname = Process.GetProcessesByName(process_name);
+            if (pname.Length != 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Jolly_Pop_Injector {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Mainform());
         }
     }
 }
