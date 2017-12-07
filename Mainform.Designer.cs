bc0e72cafb5d1ecc13b8337285b6a3b9bc304f2b
Austin <awilliams17411@gmail.com>
2017-12-06 21:52:05 -0600

    named controls on forms

--- Mainform.Designer.cs
+++ Mainform.Designer.cs
@@ -24,17 +24,17 @@
         /// </summary>
         private void InitializeComponent() {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.DLLPathTextBox = new System.Windows.Forms.TextBox();
+            this.ProcessTextbox = new System.Windows.Forms.TextBox();
+            this.DLLBrowseBtn = new System.Windows.Forms.Button();
+            this.BrowseProcessBtn = new System.Windows.Forms.Button();
+            this.InjectBtn = new System.Windows.Forms.Button();
+            this.SettingsBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.GithubLinkLabel = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
@@ -46,14 +46,14 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Status:";
             // 
-            // label2
+            // StatusLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "---";
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(55, 58);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(16, 13);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "---";
             // 
             // label3
             // 
@@ -73,91 +73,91 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "Process:";
             // 
-            // textBox1
+            // DLLPathTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(58, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 20);
-            this.textBox1.TabIndex = 4;
+            this.DLLPathTextBox.Location = new System.Drawing.Point(58, 6);
+            this.DLLPathTextBox.Name = "DLLPathTextBox";
+            this.DLLPathTextBox.Size = new System.Drawing.Size(151, 20);
+            this.DLLPathTextBox.TabIndex = 4;
             // 
-            // textBox2
+            // ProcessTextbox
             // 
-            this.textBox2.Location = new System.Drawing.Point(58, 31);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(151, 20);
-            this.textBox2.TabIndex = 5;
+            this.ProcessTextbox.Location = new System.Drawing.Point(58, 31);
+            this.ProcessTextbox.Name = "ProcessTextbox";
+            this.ProcessTextbox.Size = new System.Drawing.Size(151, 20);
+            this.ProcessTextbox.TabIndex = 5;
             // 
-            // button1
+            // DLLBrowseBtn
             // 
-            this.button1.Location = new System.Drawing.Point(211, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(33, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.DLLBrowseBtn.Location = new System.Drawing.Point(211, 4);
+            this.DLLBrowseBtn.Name = "DLLBrowseBtn";
+            this.DLLBrowseBtn.Size = new System.Drawing.Size(33, 23);
+            this.DLLBrowseBtn.TabIndex = 6;
+            this.DLLBrowseBtn.Text = "...";
+            this.DLLBrowseBtn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // BrowseProcessBtn
             // 
-            this.button2.Location = new System.Drawing.Point(211, 29);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(33, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "...";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BrowseProcessBtn.Location = new System.Drawing.Point(211, 29);
+            this.BrowseProcessBtn.Name = "BrowseProcessBtn";
+            this.BrowseProcessBtn.Size = new System.Drawing.Size(33, 23);
+            this.BrowseProcessBtn.TabIndex = 7;
+            this.BrowseProcessBtn.Text = "...";
+            this.BrowseProcessBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // InjectBtn
             // 
-            this.button3.Location = new System.Drawing.Point(169, 79);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Inject";
-            this.button3.UseVisualStyleBackColor = true;
+            this.InjectBtn.Location = new System.Drawing.Point(169, 79);
+            this.InjectBtn.Name = "InjectBtn";
+            this.InjectBtn.Size = new System.Drawing.Size(75, 23);
+            this.InjectBtn.TabIndex = 8;
+            this.InjectBtn.Text = "Inject";
+            this.InjectBtn.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // SettingsBtn
             // 
-            this.button4.Location = new System.Drawing.Point(88, 79);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Settings";
-            this.button4.UseVisualStyleBackColor = true;
+            this.SettingsBtn.Location = new System.Drawing.Point(88, 79);
+            this.SettingsBtn.Name = "SettingsBtn";
+            this.SettingsBtn.Size = new System.Drawing.Size(75, 23);
+            this.SettingsBtn.TabIndex = 9;
+            this.SettingsBtn.Text = "Settings";
+            this.SettingsBtn.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // ExitBtn
             // 
-            this.button5.Location = new System.Drawing.Point(7, 79);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 10;
-            this.button5.Text = "Exit";
-            this.button5.UseVisualStyleBackColor = true;
+            this.ExitBtn.Location = new System.Drawing.Point(7, 79);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 10;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
             // 
-            // linkLabel1
+            // GithubLinkLabel
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(4, 105);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(242, 13);
-            this.linkLabel1.TabIndex = 11;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://github.com/AWilliams17/Jolly-Pop-Injector";
+            this.GithubLinkLabel.AutoSize = true;
+            this.GithubLinkLabel.Location = new System.Drawing.Point(4, 105);
+            this.GithubLinkLabel.Name = "GithubLinkLabel";
+            this.GithubLinkLabel.Size = new System.Drawing.Size(242, 13);
+            this.GithubLinkLabel.TabIndex = 11;
+            this.GithubLinkLabel.TabStop = true;
+            this.GithubLinkLabel.Text = "https://github.com/AWilliams17/Jolly-Pop-Injector";
             // 
             // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(249, 126);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GithubLinkLabel);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.SettingsBtn);
+            this.Controls.Add(this.InjectBtn);
+            this.Controls.Add(this.BrowseProcessBtn);
+            this.Controls.Add(this.DLLBrowseBtn);
+            this.Controls.Add(this.ProcessTextbox);
+            this.Controls.Add(this.DLLPathTextBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StatusLabel);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.MaximizeBox = false;
@@ -172,17 +172,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label StatusLabel;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox DLLPathTextBox;
+        private System.Windows.Forms.TextBox ProcessTextbox;
+        private System.Windows.Forms.Button DLLBrowseBtn;
+        private System.Windows.Forms.Button BrowseProcessBtn;
+        private System.Windows.Forms.Button InjectBtn;
+        private System.Windows.Forms.Button SettingsBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.LinkLabel GithubLinkLabel;
     }
 }
 
--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -23,84 +23,84 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.AutoInjectCheckbox = new System.Windows.Forms.CheckBox();
+            this.SoundsCheckbox = new System.Windows.Forms.CheckBox();
+            this.CloseAfterInjectionCheckbox = new System.Windows.Forms.CheckBox();
+            this.SaveDLLCheckbox = new System.Windows.Forms.CheckBox();
+            this.SaveProcessCheckbox = new System.Windows.Forms.CheckBox();
+            this.CloseBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // AutoInjectCheckbox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 12);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(117, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Automatically Inject";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.AutoInjectCheckbox.AutoSize = true;
+            this.AutoInjectCheckbox.Location = new System.Drawing.Point(12, 12);
+            this.AutoInjectCheckbox.Name = "AutoInjectCheckbox";
+            this.AutoInjectCheckbox.Size = new System.Drawing.Size(117, 17);
+            this.AutoInjectCheckbox.TabIndex = 0;
+            this.AutoInjectCheckbox.Text = "Automatically Inject";
+            this.AutoInjectCheckbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // SoundsCheckbox
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(12, 58);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(85, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Play Sounds";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.SoundsCheckbox.AutoSize = true;
+            this.SoundsCheckbox.Location = new System.Drawing.Point(12, 58);
+            this.SoundsCheckbox.Name = "SoundsCheckbox";
+            this.SoundsCheckbox.Size = new System.Drawing.Size(85, 17);
+            this.SoundsCheckbox.TabIndex = 1;
+            this.SoundsCheckbox.Text = "Play Sounds";
+            this.SoundsCheckbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // CloseAfterInjectionCheckbox
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(12, 35);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(120, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Close After Injection";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.CloseAfterInjectionCheckbox.AutoSize = true;
+            this.CloseAfterInjectionCheckbox.Location = new System.Drawing.Point(12, 35);
+            this.CloseAfterInjectionCheckbox.Name = "CloseAfterInjectionCheckbox";
+            this.CloseAfterInjectionCheckbox.Size = new System.Drawing.Size(120, 17);
+            this.CloseAfterInjectionCheckbox.TabIndex = 2;
+            this.CloseAfterInjectionCheckbox.Text = "Close After Injection";
+            this.CloseAfterInjectionCheckbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // SaveDLLCheckbox
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(12, 81);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(118, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "Save DLL Location";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.SaveDLLCheckbox.AutoSize = true;
+            this.SaveDLLCheckbox.Location = new System.Drawing.Point(12, 81);
+            this.SaveDLLCheckbox.Name = "SaveDLLCheckbox";
+            this.SaveDLLCheckbox.Size = new System.Drawing.Size(118, 17);
+            this.SaveDLLCheckbox.TabIndex = 3;
+            this.SaveDLLCheckbox.Text = "Save DLL Location";
+            this.SaveDLLCheckbox.UseVisualStyleBackColor = true;
             // 
-            // checkBox5
+            // SaveProcessCheckbox
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(12, 104);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(123, 17);
-            this.checkBox5.TabIndex = 4;
-            this.checkBox5.Text = "Save Process Name";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.SaveProcessCheckbox.AutoSize = true;
+            this.SaveProcessCheckbox.Location = new System.Drawing.Point(12, 104);
+            this.SaveProcessCheckbox.Name = "SaveProcessCheckbox";
+            this.SaveProcessCheckbox.Size = new System.Drawing.Size(123, 17);
+            this.SaveProcessCheckbox.TabIndex = 4;
+            this.SaveProcessCheckbox.Text = "Save Process Name";
+            this.SaveProcessCheckbox.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // CloseBtn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 127);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Close";
-            this.button1.UseVisualStyleBackColor = true;
+            this.CloseBtn.Location = new System.Drawing.Point(12, 127);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(169, 23);
+            this.CloseBtn.TabIndex = 5;
+            this.CloseBtn.Text = "Close";
+            this.CloseBtn.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(193, 157);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.SaveProcessCheckbox);
+            this.Controls.Add(this.SaveDLLCheckbox);
+            this.Controls.Add(this.CloseAfterInjectionCheckbox);
+            this.Controls.Add(this.SoundsCheckbox);
+            this.Controls.Add(this.AutoInjectCheckbox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SettingsForm";
             this.Text = "Jolly-Pop Injector Settings";
@@ -111,11 +111,11 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox AutoInjectCheckbox;
+        private System.Windows.Forms.CheckBox SoundsCheckbox;
+        private System.Windows.Forms.CheckBox CloseAfterInjectionCheckbox;
+        private System.Windows.Forms.CheckBox SaveDLLCheckbox;
+        private System.Windows.Forms.CheckBox SaveProcessCheckbox;
+        private System.Windows.Forms.Button CloseBtn;
     }
 }
