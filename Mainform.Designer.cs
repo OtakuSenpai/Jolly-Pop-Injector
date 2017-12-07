9a8078287d9752f8509d09d22bbb2db5100c1d85
Austin <awilliams17411@gmail.com>
2017-12-06 19:53:10 -0600

    added classes

--- Mainform.Designer.cs
+++ Mainform.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Jolly_Pop_Injector {
-    partial class Form1 {
+    partial class Mainform {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,9 +23,17 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 246);
+            this.Name = "Mainform";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Mainform.cs
+++ Mainform.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Jolly_Pop_Injector {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class Mainform : Form {
+        public Mainform() {
             InitializeComponent();
         }
     }
