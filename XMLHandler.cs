3051c033188174b90cd3ce377890bf31592326bb
Austin <awilliams17411@gmail.com>
2017-12-08 16:29:42 -0600

    replaced the horrible xmlhandler code with the better xmlhandler code from halo ce mouse tool.

--- XMLHandler.cs
+++ XMLHandler.cs
@@ -1,210 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.IO;
-using System.Xml;
+using System.Xml.Serialization;
 
+namespace Jolly_Pop_Injector
+{
+    /*
+        -XMLHandler.cs-
+        This class handles the applications XML file.
+    */
+    public class XMLHandler
+    {
+        public static string XMLPath;
 
-namespace Jolly_Pop_Injector {
-    public class XMLHandler {
-        private static string XMLPath = Application.StartupPath + "/JPI.xml";
+        public XMLHandler(string xmlpath)
+        {
+            XMLPath = xmlpath;
+        }
 
-        private static bool XMLExists() { //Check if the XML configuration file exists.
-            if (File.Exists(XMLPath)) {
-                return true;
-            } else {
-                return false;
+        public static bool XMLExists()
+        { //Check if the XML configuration file exists.
+            return File.Exists(XMLPath);
+        }
+        public static int Serialize_Settings(SettingsHandler settings) //For context, if I am calling it in processexit, the context will be 0. Otherwise, it will be 1.
+        {
+            try
+            {
+                XmlSerializer SerializerObj = new XmlSerializer(typeof(SettingsHandler));
+                TextWriter WriteFileStream = new StreamWriter(@XMLPath);
+                SerializerObj.Serialize(WriteFileStream, settings);
+                WriteFileStream.Close();
+                return 1; //Success
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0; //Exception occurred. Let the caller handle it.
             }
         }
 
-        public static void GenerateXML(SettingsHandler settings) { //Used for generating a new XML file with hardcoded default values.
-            try {
-                XmlWriter xmlWriter = XmlWriter.Create("JPI.xml");
-
-                xmlWriter.WriteStartDocument();
-                xmlWriter.WriteStartElement("JPI");
-
-                xmlWriter.WriteStartElement("JPISettings");
-                xmlWriter.WriteAttributeString("AutoInject", "1");
-                xmlWriter.WriteAttributeString("CloseAfterInjection", "1");
-                xmlWriter.WriteAttributeString("SaveProcessName", "1");
-                xmlWriter.WriteAttributeString("SaveDLLlocation", "1");
-                xmlWriter.WriteAttributeString("DLL", "not set");
-                xmlWriter.WriteAttributeString("Process", "not set");
-
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteEndDocument();
-                xmlWriter.Close();
-            } catch (System.UnauthorizedAccessException) {
-                MessageBox.Show("Error! I can not create an XML file at this location. You do not have access. Please re-run as Admin. I will continue without writing a config.");
-            }
-        }
-
-        public static void WriteSettingsToXML(SettingsHandler settings) { //Called when program exits. Push current settings to the XML file.
-            XmlDocument doc = new XmlDocument();
-            try {
-                doc.Load(XMLPath);
-            } catch (XmlException) { //Root element missing
-                XmlElement elem = doc.CreateElement("JPI");
-                doc.AppendChild(elem);
-            }
-            //These are to determine if the XML wrote everything correctly.
-            bool AutoInjectWrote = true;
-            bool CloseAfterInjectionWrote = true;
-            bool SaveProcessNameWrote = true;
-            bool SaveDLLlocationWrote = true;
-            bool ProcessWrote = true;
-            bool DLLWrote = true;
-
-            XmlNode root = doc.DocumentElement["JPISettings"];
-            try {
-                foreach (XmlAttribute c in root.Attributes) {
-                    if (c.Name == "AutoInject") {
-                        c.Value = settings.AutoInject.ToString();
-                    } else {
-                        AutoInjectWrote = false;
-                    }
-                    if (c.Name == "CloseAfterInjection") {
-                        c.Value = settings.CloseAfterInjection.ToString();
-                    } else {
-                        CloseAfterInjectionWrote = false;
-                    }
-                    if (c.Name == "SaveProcessName") {
-                        c.Value = settings.SaveProcessName.ToString();
-                    } else {
-                        SaveProcessNameWrote = false;
-                    }
-                    if (c.Name == "SaveDLLlocation") {
-                        c.Value = settings.SaveDLLlocation.ToString();
-                    } else {
-                        SaveDLLlocationWrote = false;
-                    }
-                    if (c.Name == "Process") {
-                        c.Value = settings.Process;
-                    } else {
-                        ProcessWrote = false;
-                    }
-                    if (c.Name == "DLL") {
-                        c.Value = settings.DLL;
-                    } else {
-                        DLLWrote = false;
-                    }
+        public static SettingsHandler DeSerialize_Settings()
+        {
+            if (XMLExists())
+            {
+                XmlSerializer SerializerObj = new XmlSerializer(typeof(SettingsHandler));
+                SettingsHandler settings_loaded;
+                FileStream ReadFileStream = new FileStream(@XMLPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                try
+                {
+                    settings_loaded = (SettingsHandler)SerializerObj.Deserialize(ReadFileStream);
                 }
-            } catch (NullReferenceException) { //Occurs if JPISettings doesn't exist
-                XmlElement JPISettings = doc.CreateElement("JPISettings");
-                doc.DocumentElement.AppendChild(JPISettings);
-                AutoInjectWrote = false;
-                CloseAfterInjectionWrote = false;
-                SaveProcessNameWrote = false;
-                SaveDLLlocationWrote = false;
-                ProcessWrote = false;
-                DLLWrote = false;
-                root = doc.DocumentElement["JPISettings"]; //Reset root to JPISettings. it will be written to later.
-            }
-            //Anakin Memewalker: This is where the fun begins
-            //If any of the attributes failed to write, then remake them and try again.
-            if (!AutoInjectWrote) {
-                XmlAttribute AutoInject = doc.CreateAttribute("AutoInject");
-                AutoInject.Value = settings.AutoInject.ToString();
-                root.Attributes.SetNamedItem(AutoInject);
-            }
-            if (!CloseAfterInjectionWrote) {
-                XmlAttribute CloseAfterInjection = doc.CreateAttribute("CloseAfterInjection");
-                CloseAfterInjection.Value = settings.CloseAfterInjection.ToString();
-                root.Attributes.SetNamedItem(CloseAfterInjection);
-            }
-            if (!SaveProcessNameWrote) {
-                XmlAttribute SaveProcessName = doc.CreateAttribute("SaveProcessName");
-                SaveProcessName.Value = settings.SaveProcessName.ToString();
-                root.Attributes.SetNamedItem(SaveProcessName);
-            }
-            if (!SaveDLLlocationWrote) {
-                XmlAttribute SaveDLLlocation = doc.CreateAttribute("SaveDLLlocation");
-                SaveDLLlocation.Value = settings.SaveDLLlocation.ToString();
-                root.Attributes.SetNamedItem(SaveDLLlocation);
-            }
-            if (!ProcessWrote) {
-                XmlAttribute Process = doc.CreateAttribute("Process");
-                Process.Value = settings.Process.ToString();
-                root.Attributes.SetNamedItem(Process);
-            }
-            if (!DLLWrote) {
-                XmlAttribute DLL = doc.CreateAttribute("DLL");
-                DLL.Value = settings.DLL.ToString();
-                root.Attributes.SetNamedItem(DLL);
-            }
-            doc.Save(XMLPath);
-        }
-        /*
-            Rob say Code Monkey very dilligent - but his output stink.
-            His code not functional or elegant.
-            What do code monkey think?
-        */
-        public static int LoadSettingsFromXML(SettingsHandler s) {
-            bool err = false; //If an error occurs during the loading, set this to true and leave the setting to its default value.
-            if (XMLExists()) {
-                //Load and set values
-                XmlReader xmlReader = XmlReader.Create(XMLPath);
-                try {
-                    while (xmlReader.Read()) {
-                        if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "JPISettings")) {
-                            if (xmlReader.HasAttributes) {
-                                int AutoInject;
-                                int CloseAfterInjection;
-                                int SaveProcessName;
-                                int SaveDLLlocation;
-                                string Process;
-                                string DLL;
-                                if (!int.TryParse(xmlReader.GetAttribute("AutoInject"), out AutoInject)) {
-                                    err = true;
-                                } else {
-                                    s.AutoInject = AutoInject;
-                                }
-                                if (!int.TryParse(xmlReader.GetAttribute("CloseAfterInjection"), out CloseAfterInjection)) {
-                                    err = true;
-                                } else {
-                                    s.CloseAfterInjection = CloseAfterInjection;
-                                }
-                                if (!int.TryParse(xmlReader.GetAttribute("SaveProcessName"), out SaveProcessName)) {
-                                    err = true;
-                                } else {
-                                    s.SaveProcessName = SaveProcessName;
-                                }
-                                if (!int.TryParse(xmlReader.GetAttribute("SaveDLLlocation"), out SaveDLLlocation)) {
-                                    err = true;
-                                } else {
-                                    s.SaveDLLlocation = SaveDLLlocation;
-                                }
-                                DLL = xmlReader.GetAttribute("DLL");
-                                Process = xmlReader.GetAttribute("Process");
-                                if (DLL != null) {
-                                    s.DLL = DLL;
-                                } else {
-                                    err = true;
-                                }
-                                if (Process != null) {
-                                    s.Process = Process;
-                                } else {
-                                    err = true;
-                                }
-                                xmlReader.Close();
-                            }
-                        }
-                    }
-                } catch (XmlException) {
-                    return 2; //2 == Exception
+                catch (InvalidOperationException) //Seems to occur if the xml file for some reason has been screwed up.
+                {
+                    settings_loaded = null; //Failed to load it. return null. I will leave what to do with the corrupt config file up to the user.
                 }
-                if (err == true) {
-                    return 3; //3 == One or more values not read properly; they are set to default values
+                finally //Always close the filestream no matter the outcome.
+                {
+                    ReadFileStream.Close();
                 }
-                return 1; //1 == Successfully read
-            } else {
-                return 0; //0 == didn't find an XML
+                return settings_loaded;
             }
-            //TODO: Make this class not shit.
+            return null; //It doesn't exist. Return null.
         }
     }
 }
