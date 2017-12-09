4bf98b1f3660cac6f9aa9d660f96766f67236f15
Austin <awilliams17411@gmail.com>
2017-12-09 08:06:31 -0600

    unneeded usings removed

--- DLLInjector.cs
+++ DLLInjector.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using System.Diagnostics;
-using System.IO;
 
 
 namespace Jolly_Pop_Injector
